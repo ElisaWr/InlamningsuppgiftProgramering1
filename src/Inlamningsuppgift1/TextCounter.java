d8b18d71e3b9c4f7cb1a314f41c668a7185c4fce
ElisaWr <elisa.wretling@gmail.com>
2023-11-05 23:31:02 +0100

    Update TextCounter.java
    removed irrelevant comments

--- src/Inlamningsuppgift1/TextCounter.java
+++ src/Inlamningsuppgift1/TextCounter.java
@@ -27,13 +27,11 @@ public class TextCounter {
 
     public void addTextLine(String text) {
 
-        //System.out.println(textEntry); //TESTLINE
         lineCount = lineCount+1;
         textEntry = textEntry + text;
     }
 
     public int getNumberOfLines(){
-        //System.out.println("Du har skrivit in "+ (lineCount-1) +" textrader");
         return lineCount-1;
     }
 
@@ -41,12 +39,9 @@ public class TextCounter {
     public int getNumberOfCharacters(){
         int charCount = textEntry.length();
         charCount = charCount-endCommandLength;
-        //System.out.println("Du har använt "+ charCount +" tecken");
-        //System.out.println(textEntry); //TESTLINE
         return charCount;
     }
     public String getTotalText(){
-        //System.out.println("Du har skrivit in "+ (lineCount-1) +" textrader");
         return textEntry;
     }
 
